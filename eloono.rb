41fe7fb5afe726e25ec72fddc3bf501084b9ece6
raleighgee <raleigh.gresham@gmail.com>
2013-03-25 11:23:14 -0400

    fix ellono.rb 3 and add model files

--- eloono.rb
+++ eloono.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'config/environments.rb'
-require 'models/model.rb'
+require 'models/connection.rb'
 
 get '/' do
   "Just Checking it Out"
