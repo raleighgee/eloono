10a681f702cb3b494eedd027e6025c8aee116c00
raleighgee <raleigh.gresham@gmail.com>
2013-03-25 11:34:42 -0400

    fix ellono.rb 5

--- eloono.rb
+++ eloono.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
-require 'environments.rb'
-require 'connection.rb'
+require '../environments.rb'
+require '../connection.rb'
 
 get '/' do
   "Just Checking it Out"
