14f33588d7b86e3e4706788a681a0b0221a92a85
raleighgee <raleigh.gresham@gmail.com>
2013-03-25 12:36:31 -0400

    move db config our of enviro files

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "http://rubygems.org"
 gem 'sinatra'
 gem "activerecord"
+gem "uri"
 gem "sinatra-activerecord"
 gem "pg"
--- eloono.rb
+++ eloono.rb
@@ -1,7 +1,21 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
-require_relative 'config/environments'
+require 'active_record'
+require 'uri'
+
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :port     => db.port,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
+)
+
 
 
 get '/' do
