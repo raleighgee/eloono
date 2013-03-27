794a63829f7360780f0c5542cd307d11fd0f6e15
raleighgee <gee@raleighgresham.com>
2013-03-27 16:01:24 -0400

    initial word and link creation try 1

--- eloono.rb
+++ eloono.rb
@@ -178,6 +178,137 @@ get '/first_score' do
 					
 			t.save
 			
+			# split words in Tweet up using spaces
+			@words = t.tweet_content.split(" ")
+
+			# reset cleantweet variable instance
+			cleantweet = ""
+				
+			# begin looping through words in tweet
+			@words.each do |w|
+				unless w.include? %{http}
+					unless w.include? %{@}
+						unless w.is_a? (Numeric)
+							unless w == ""
+								# remove any non alphanumeric charactes from word
+								cleanword = w.gsub(/[^0-9a-z]/i, '')
+								# set all characters to lowercase
+								cleanword = cleanword.downcase
+								# look to see if word already exists, if not, create a new one using cleanword above
+								word = Word.find_or_create_by_word_and_user_id(:word => cleanword, :user_id => user.id)
+								# check if word is on the System ignore list
+								sysignore = Sysigword.find_by_word(cleanword)
+								if sysignore
+									word.sys_ignore_flag = "yes"
+								end
+								# increment the number of times word has been seen counter by 1
+								word.seen_count = word.seen_count.to_i+1
+								word.save
+							end # End check if word is empty
+						end # End check if word is just a number
+					end # End check if word contains the @ symbol
+				end # End check if word is a link
+				
+				# if the number of words in the tweet is less than 3, set the tweet content to exactly what the tweet says - no clean required
+				if @words.size < 3
+					cleantweet = t.tweet_content
+				else
+					# build clean version of tweet
+  					if w.include? %{http}
+						cleantweet = cleantweet.to_s+%{[...] }
+					elsif w.include? %{@}
+						firstchar = w[0,1]
+						secondchar = w[1,1]
+						if firstchar == %{@} or secondchar == %{@}
+							if w.length.to_i > 1
+								nohandle = w.gsub('@', '')
+								nohandle = nohandle.gsub(" ", '')
+								nohandle = nohandle.gsub(":", '')
+								nohandle = nohandle.gsub(";", '')
+								nohandle = nohandle.gsub(",", '')
+								nohandle = nohandle.gsub(".", '')
+								nohandle = nohandle.gsub(")", '')
+								nohandle = nohandle.gsub("(", '')
+								nohandle = nohandle.gsub("*", '')
+								nohandle = nohandle.gsub("^", '')
+								nohandle = nohandle.gsub("$", '')
+								nohandle = nohandle.gsub("#", '')
+								nohandle = nohandle.gsub("!", '')
+								nohandle = nohandle.gsub("~", '')
+								nohandle = nohandle.gsub("`", '')
+								nohandle = nohandle.gsub("+", '')
+								nohandle = nohandle.gsub("=", '')
+								nohandle = nohandle.gsub("[", '')
+								nohandle = nohandle.gsub("]", '')
+								nohandle = nohandle.gsub("{", '')
+								nohandle = nohandle.gsub("}", '')
+								nohandle = nohandle.gsub("/", '')
+								nohandle = nohandle.gsub("<", '')
+								nohandle = nohandle.gsub(">", '')
+								nohandle = nohandle.gsub("?", '')
+								nohandle = nohandle.gsub("&", '')
+								nohandle = nohandle.gsub("|", '')
+								nohandle = nohandle.gsub("-", '')
+								#nohandle = nohandle.gsub('"','')
+								cleantweet = cleantweet.to_s+%{<a href="http://twitter.com/}+nohandle.to_s+%{" target="_blank" class="embed_handle">}+w.to_s+%{</a> }
+							else
+								cleantweet = cleantweet.to_s+w.to_s+" "
+							end
+						else
+							cleantweet = cleantweet.to_s+w.to_s+" "
+						end
+					elsif w.include? %{#}
+						firstchar = w[0,1]
+						secondchar = [1,1]
+						if firstchar == %{#} or secondchar == %{#}
+							nohandle = w.gsub('#', '')
+							cleantweet = cleantweet.to_s+%{<a href="https://twitter.com/search/}+nohandle.to_s+%{" target="_blank" class="embed_handle">}+w.to_s+%{</a> }
+						else
+							cleantweet = cleantweet.to_s+w.to_s+" "
+						end
+					else
+						cleantweet = cleantweet.to_s+w.to_s+" "
+					end
+				end # End check if tweet is smaller than 3 words
+			end # End create words
+				
+			# set clean_tweet_content version of tweet content
+			t.clean_tweet_content = cleantweet.to_s
+
+			# make sure tweet has at least a 0 word quailty score if it is blank
+			if t.word_quality_score == "NULL" || t.word_quality_score == ""
+				t.word_quality_score = t.word_quality_score.to_i+0
+			end
+
+			# Check if tweet has a link, and if so, build out links
+			if t.tweet_content.include? %{http}
+				t.tweet_type = "link"
+				@flinks = t.tweet_content.split("http")
+				for flink in @flinks
+					if flink.include? %{://}
+						@reallinks = flink.split(" ")
+						boom = %{http}+@reallinks[0].to_s
+						boom = boom.gsub('"','')
+						l = Link.find_or_create_by_expanded_url_and_tweet_id_and_user_id(:tweet_id => t.id, :expanded_url => boom, :user_id => user.id, :source_id => t.source_id)
+						l.save
+					end
+				end
+			end # End Create links
+				
+			# Set the tweet type based on whether the tweet has a link or not
+			haslink = Link.find_by_tweet_id(t.id)
+			if haslink
+				flinks = Link.count(:all, :conditions => ["tweet_id = ?", t.id])
+				t.tweet_type = "link"
+				t.url_count = flinks
+			else
+				t.tweet_type = "non-link"
+			end
+		
+			t.last_action = "new"
+		
+			t.save
+			
 			# increase current user's number of tweets by 1
 			tweets = Tweet.count(:conditions => ["user_id = ?", user.id])
 			itweets = Itweets.count(:conditions => ["user_id = ?", user.id])
