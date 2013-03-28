e4ac7976323b762ab88507220bfaeb77a2aff834
raleighgee <gee@raleighgresham.com>
2013-03-28 12:39:57 -0400

    add interaction code

--- eloono.rb
+++ eloono.rb
@@ -546,7 +546,7 @@ get '/tweets' do
 	
 	@nonlinks = ""
 	for nonlinktweet in @nonlinktweets
-		@nonlinks = @nonlinks.to_s+nonlinktweet.clean_tweet_content.to_s+%{<br />}
+		@nonlinks = @nonlinks.to_s+nonlinktweet.clean_tweet_content.to_s+%{ | <a href="http://eloono.com/interact/}+linktweet.id.to_s+%{" target="_blank">Join Conversation</a><br />}
 	end
 	
 	%{<h1>Top 25 Link Tweets</h1>}+@links.to_s+%{<br /><br /><h1>Top 25 Non-Link Tweets</h1>}+@nonlinks.to_s
@@ -615,6 +615,70 @@ get '/follow/:t' do
 	end
 	
 end
+
+
+get '/interact/:t/' do
+	tweet = Tweet.find_by_id(params[:t])
+	
+	if tweet
+		if tweet.followed_flag != "yes"
+			source = Source.find_by_id(tweet.source_id)
+			source.number_links_followed = source.number_links_followed.to_i+1
+			source.number_of_interactions = source.number_of_interactions.to_i+1
+			source.save
+			@words = tweet.tweet_content.split(" ")
+			@words.each do |w|
+				cleanword = w.gsub(/[^0-9a-z]/i, '')
+				cleanword = cleanword.downcase
+				word = Word.find(:first, :conditions => ["word = ? and user_id = ? and sys_ignore_flag <> ?", cleanword, tweet.user_id, "yes"])
+				if word
+					word.score = word.score.to_i+1
+					word.follows = word.follows.to_i+1
+					if word.word.include? %{#}
+						word.score = word.score.to_i+2
+					end
+					word.comp_average = (word.score.to_f+word.follows.to_f+word.seen_count.to_f)/3
+					word.save
+				end # End check if word is exists
+			end # End loop through words
+			tweet.followed_flag = "yes"
+			tweet.last_action = "tweeted"
+			tweet.save
+			
+			linkcode = %{https://twitter.com/intent/tweet?in_reply_to=}+tweet.twitter_id.to_s+%{&via=}+tweet.source.user_screen_name.to_s
+
+			i = Itweets.find_or_create_by_user_id_and_twitter_id(:user_id => tweet.user_id, :twitter_id => tweet.twitter_id)
+			i.old_id = tweet.id
+			i.source_id = tweet.source_id
+			i.score = tweet.score
+			i.tweet_type = tweet.tweet_type
+			i.url_count = tweet.url_count
+			i.followed_flag = tweet.followed_flag
+			i.last_action = tweet.last_action
+			i.twitter_created_at = tweet.twitter_created_at
+			i.retweet_count = tweet.retweet_count
+			i.tweet_source = tweet.tweet_source
+			i.tweet_content = tweet.tweet_content
+			i.clean_tweet_content = tweet.clean_tweet_content
+			i.truncated_flag = tweet.truncated_flag
+			i.reply_id = tweet.reply_id
+			i.convo_flag = tweet.convo_flag
+			i.convo_initiator = tweet.convo_initiator
+			i.word_quality_score = tweet.word_quality_score
+			i.source_score_score = tweet.source_score_score
+			i.old_created_at = tweet.created_at
+			i.save
+			tweet.destroy
+		end # end check if tweet has already been followed
+	else # if tweet is not found then...
+		itweet = Itweet.find_by_old_id(params[:t])
+		linkcode = %{https://twitter.com/intent/tweet?in_reply_to=}+itweet.twitter_id.to_s+%{&via=}+itweet.source.user_screen_name.to_s
+	end # end check if a tweet is found
+	
+	redirect_to linkcode
+	
+end
+
 
 
 get '/sendmail' do
