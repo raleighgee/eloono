d4b1bab8b61d66e8b793a4576fe201ab7b298bb3
rals0381 <raleigh.gresham@gmail.com>
2013-04-01 10:05:09 -0400

    swith to no time limit on sendmail for testing

--- sendmail.rb
+++ sendmail.rb
@@ -36,7 +36,7 @@ ActiveRecord::Base.establish_connection(
 
 @users = User.find(:all, :conditions => ["active_scoring <> ?", "yes"])
 for user in @users
-  sixago = Time.now-(5*60*60)
+  sixago = Time.now#-(5*60*60)
   if user.last_interaction <= sixago
     
     @linktweets = Tweet.find(:all, :conditions => ["user_id = ? and tweet_type = ? and last_action = ?", user.id, "link", "scored"], :order => "score DESC, updated_at DESC", :limit => 15)
@@ -132,7 +132,7 @@ for user in @users
   	end
 
     # Build list of top ten words for review
-    topwords= ""
+    topwords = ""
     @toptenwords = Topword.find(:all, :conditions => ["user_id = ?", user.id], :order => "rank DESC", :limit => 10)
     for toptenword in @toptenwords
       topwords = topwords.to_s+toptenword.to_s+%{ | <a href="http://eloono.com/ats/}+toptenword.to_s+%{" target="_blank">Remove</a><br /><br />}
