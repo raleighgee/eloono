e0fbe3416848c0ba2ca8ed53fcd9cd3c59987789
rals0381 <raleigh.gresham@gmail.com>
2013-04-05 16:31:02 -0400

    change field types in word to float

--- db/migrate/20120626014316_create_words.rb
+++ db/migrate/20120626014316_create_words.rb
@@ -2,9 +2,9 @@ class CreateWords < ActiveRecord::Migration
   def change
     create_table :words do |t|
       t.string :word
-      t.integer :seen_count, :default => 0
-      t.integer :follows, :default => 0
-      t.integer :score, :default => 0
+      t.float :seen_count, :default => 0
+      t.float :follows, :default => 0
+      t.float :score, :default => 0
 	    t.float :comp_average, :default => 0
       t.integer :user_id
       t.string :sys_ignore_flag, :default => "no"
