c7eb0a620a424940bda9d1319278acc69d25184c
rals0381 <raleigh.gresham@gmail.com>
2013-05-18 12:23:17 -0400

    add back migrations to clean out DB

---
+++ db/migrate/20120626033148_create_connections.rb
@@ -0,0 +1,16 @@
+class CreateConnections < ActiveRecord::Migration
+	def change
+		create_table :connections do |t|
+			t.integer :source_id
+			t.integer :tweet_id
+			t.integer :user_id
+			t.string :user_screen_name, :default => "wait"
+			t.string :profile_image_url, :default => "wait"
+			t.integer :num_appears, :default => 1
+			t.float :avg_assoc_tweet_score, :default => 0
+			t.text :user_description, :default => "wait"
+			t.string :profile_image_url, :default => "wait"
+			t.timestamps
+		end
+	end
+end
---
+++ db/migrate/20120701171708_create_users.rb
@@ -0,0 +1,28 @@
+class CreateUsers < ActiveRecord::Migration
+	def change
+		create_table :users do |t|
+			t.string :provider
+			t.string :name
+			t.string :handle
+			t.string :email
+			t.string :profile_image_url
+			t.string :secret, :default => "0"
+			t.string :token, :default => "0"
+			t.integer :uid, :limit => 8
+			t.integer :num_score_rounds, :default => 0
+			t.integer :level, :default => 0
+			t.string :language, :default => "en"
+			t.integer :calls_left, :default => 350
+			t.integer :number_eloonos_sent, :default => 0
+			t.integer :num_tweets_pulled, :default => 0
+			t.string :active_scoring, :default => "no"
+			t.string :pay_key, :default => "0"
+			t.string :sorting_by, :default => "age"
+			t.float :amount_owed, :default => 0
+			t.string :payment_status, :default => "paid"
+			t.datetime :last_interaction, :default => Time.now
+			t.float :current_score_threshold, :default => 0
+			t.timestamps
+		end
+	end
+end
