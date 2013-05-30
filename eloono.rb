86c252d1d99971093bd6184f8f709df5409d802a
rals0381 <raleigh.gresham@gmail.com>
2013-05-29 23:16:04 -0400

    try 6

--- eloono.rb
+++ eloono.rb
@@ -119,7 +119,7 @@ get '/words/:id/:action' do
     
 end
 
-get '/con_rec/:con/:user/:action' do
+get '/conrec/:con/:user/:action' do
   connection = Connection.find(:first, :conditions => ["user_id = ? and id = ?", params[:user], params[:con]])
   if connection
     if params[:action] == "follow"
@@ -134,79 +134,4 @@ get '/con_rec/:con/:user/:action' do
   else
     %{Hmmmmm. Can't seem to find that recommendation. Sorry about that!}
   end  
-end
-
-get '/test_rec_email' do
-  
-	@users = User.find(:all)
-	
-	@connections = Connection.find(:all, :conditions => ["connection_type = ?", "reccomended"])
-	for connection in @connections
-	  connection.connection_type = "mentioned"
-	  connection.save
-	end
-	
-	for user in @users
-	  # Authenticate user for pulling of Tweets
-		Twitter.configure do |config|
-			config.consumer_key = "DHBxwGvab2sJGw3XhsEmA"
-			config.consumer_secret = "530TCO6YMRuB23R7wse91rTcIKFPKQaxFQNVhfnk"
-			config.oauth_token = user.token
-			config.oauth_token_secret = user.secret
-		end
-		body = ""
-		@connections = Connection.find(:all, :conditions => ["user_id = ? and connection_type = ?", user.id, "mentioned"], :limit => 5, :order => "average_stream_word_score DESC")
-		for connection in @connections
-			c = Twitter.user(connection.user_screen_name)
-			if c
-				connection.twitter_id = c.id
-				connection.profile_image_url = c.profile_image_url
-				connection.user_name = c.name
-				connection.following_flag = c.following
-				connection.user_description = c.description
-				connection.user_url = c.url
-				connection.user_language = c.lang
-				connection.location = c.location
-				connection.twitter_created_at = c.created_at
-				connection.statuses_count = c.statuses_count
-				connection.followers_count = c.followers_count
-				connection.friends_count = c.friends_count
-				connection.connection_type = "reccomended"
-				ageinhours = ((Time.now-c.created_at)/60)/60
-				connection.tweets_per_hour = c.statuses_count.to_f/ageinhours.to_f
-				connection.save
-			end # end check if Twitter can find this connection
-			
-			ageinyears = ((((Time.now-connection.twitter_created_at)/60)/60)/24)/365
-  		ftofratio = connection.friends_count.to_f/connection.followers_count.to_f
-			
-			body = body.to_s+%{<div style="text-align:center;"><h3>}+connection.user_screen_name.to_s+%{</h3><a href="}+connection.user_url.to_s+%{" target="_blank"><img src="}+connection.profile_image_url.to_s+%{" width="48" hegith="48" /></a><p>This is <b>}+connection.user_name.to_s+%{</b> - <i>}+connection.user_description.to_s+%{</i> They speak <b>}+connection.user_language.upcase.to_s+%{</b> and are located in <b>}+connection.location.to_s+%{</b>. They have been on Twitter for <b>}+ageinyears.to_f.round.to_s+%{</b> years and have sent <b>}+connection.statuses_count.to_f.round.to_s+%{</b> tweets at a rate of <b>}+connection.tweets_per_hour.to_f.round(2).to_s+%{</b> tweets per hour. They have <b>}+connection.friends_count.to_f.round.to_s+%{</b> friends and <b>}+connection.followers_count.to_f.round.to_s+%{</b> followers.</p><h5><a href="http://eloono.com/conrec/}+connection.id.to_s+%{/}+user.id.to_s+%{/follow" target="_blank">Follow</a> | <a href="http://eloono.com/conrec/}+connection.id.to_s+%{/}+user.id.to_s+%{/ignore" target="_blank">Ignore</a></h5></div><br />}
-			
-		end
-		
-		Pony.mail(
-		  :headers => {'Content-Type' => 'text/html'},
-		  :from => 'recommendations@eloono.com',
-		  :to => 'raleigh.gresham@gmail.com',
-		  :subject => 'Some people you might want to connect with.',
-		  :body => body.to_s,
-		  :port => '587',
-		  :via => :smtp,
-		  :via_options => { 
-			:address => 'smtp.sendgrid.net', 
-			:port => '587', 
-			:enable_starttls_auto => true, 
-			:user_name => ENV['SENDGRID_USERNAME'], 
-			:password => ENV['SENDGRID_PASSWORD'], 
-			:authentication => :plain, 
-			:domain => ENV['SENDGRID_DOMAIN']
-		  }
-		)  
-		
-		#user.last_connectionsemail = Time.now
-		#user.save
-	end
-
-  %{Boom complete}
-	
 end
--- processes.rb
+++ processes.rb
@@ -617,7 +617,57 @@ for user in @users
 
   ###### SEND CONNECTIONS REC. EMAIL - WEEKLY ######
   if user.last_connectionsemail <= (Time.now-(7*24*60*60))
-	
+		body = ""
+		@connections = Connection.find(:all, :conditions => ["user_id = ? and connection_type = ?", user.id, "mentioned"], :limit => 5, :order => "average_stream_word_score DESC")
+		for connection in @connections
+			c = Twitter.user(connection.user_screen_name)
+			if c
+				connection.twitter_id = c.id
+				connection.profile_image_url = c.profile_image_url
+				connection.user_name = c.name
+				connection.following_flag = c.following
+				connection.user_description = c.description
+				connection.user_url = c.url
+				connection.user_language = c.lang
+				connection.location = c.location
+				connection.twitter_created_at = c.created_at
+				connection.statuses_count = c.statuses_count
+				connection.followers_count = c.followers_count
+				connection.friends_count = c.friends_count
+				connection.connection_type = "reccomended"
+				ageinhours = ((Time.now-c.created_at)/60)/60
+				connection.tweets_per_hour = c.statuses_count.to_f/ageinhours.to_f
+				connection.save
+			end # end check if Twitter can find this connection
+			
+			ageinyears = ((((Time.now-connection.twitter_created_at)/60)/60)/24)/365
+  		ftofratio = connection.friends_count.to_f/connection.followers_count.to_f
+			
+			body = body.to_s+%{<div style="text-align:center;"><h3>}+connection.user_screen_name.to_s+%{</h3><a href="}+connection.user_url.to_s+%{" target="_blank"><img src="}+connection.profile_image_url.to_s+%{" width="48" hegith="48" /></a><p>This is <b>}+connection.user_name.to_s+%{</b> - <i>}+connection.user_description.to_s+%{</i> They speak <b>}+connection.user_language.upcase.to_s+%{</b> and are located in <b>}+connection.location.to_s+%{</b>. They have been on Twitter for <b>}+ageinyears.to_f.round.to_s+%{</b> years and have sent <b>}+connection.statuses_count.to_f.round.to_s+%{</b> tweets at a rate of <b>}+connection.tweets_per_hour.to_f.round(2).to_s+%{</b> tweets per hour. They have <b>}+connection.friends_count.to_f.round.to_s+%{</b> friends and <b>}+connection.followers_count.to_f.round.to_s+%{</b> followers.</p><h5><a href="http://eloono.com/conrec/}+connection.id.to_s+%{/}+user.id.to_s+%{/follow" target="_blank">Follow</a> | <a href="http://eloono.com/conrec/}+connection.id.to_s+%{/}+user.id.to_s+%{/ignore" target="_blank">Ignore</a></h5></div><br />}
+			
+		end
+		
+		Pony.mail(
+		  :headers => {'Content-Type' => 'text/html'},
+		  :from => 'recommendations@eloono.com',
+		  :to => 'raleigh.gresham@gmail.com',
+		  :subject => 'Some people you might want to connect with.',
+		  :body => body.to_s,
+		  :port => '587',
+		  :via => :smtp,
+		  :via_options => { 
+			:address => 'smtp.sendgrid.net', 
+			:port => '587', 
+			:enable_starttls_auto => true, 
+			:user_name => ENV['SENDGRID_USERNAME'], 
+			:password => ENV['SENDGRID_PASSWORD'], 
+			:authentication => :plain, 
+			:domain => ENV['SENDGRID_DOMAIN']
+		  }
+		)  
+		
+		user.last_connectionsemail = Time.now
+		user.save
 
 	
   end # end check if last connections email was at least 7 days ago
