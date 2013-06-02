dbf3018812e0c9c75d616ad293375491c53ffc30
rals0381 <raleigh.gresham@gmail.com>
2013-06-02 16:59:28 -0400

    try 2

--- eloono.rb
+++ eloono.rb
@@ -85,9 +85,9 @@ get '/word_review' do
   user = User.find_by_id(session[:user_id])
   if user
     wordcode = ""
-    upcode = %{<div style="position:fixed; top:33px; left:233px;"><h4>Here are the words you have tumbed up:</h4>}
+    upcode = %{<div style="position:fixed; top:53px; left:233px;"><h4>Here are the words you have tumbed up:</h4>}
     word = Word.find(:first, :conditions => ["user_id = ? and thumb_status = ? and sys_ignore_flag = ?", session[:user_id], "neutral", "no"], :order => "score DESC")
-    wordcode = wordcode.to_s+%{<span style="font-size:2.5em; font-family:Helvetica;">}+word.word.to_s+%{</span> | <a style="font-size:1.2em;" href="http://eloono.com/words/}+word.id.to_s+%{/up?src=page">+</a> | <a style="font-size:1.2em;" href="http://eloono.com/words/}+word.id.to_s+%{/down?src=page">-</a> | <a style="font-size:1.2em;" href="http://eloono.com/words/}+word.id.to_s+%{/ignore?src=page">x</a><br /><br />}
+    wordcode = wordcode.to_s+%{<span style="font-size:2.5em; font-family:Helvetica;">}+word.word.to_s+%{</span><br /><a style="font-size:1.2em;" href="http://eloono.com/words/}+word.id.to_s+%{/up?src=page">+</a> | <a style="font-size:1.2em;" href="http://eloono.com/words/}+word.id.to_s+%{/down?src=page">-</a> | <a style="font-size:1.2em;" href="http://eloono.com/words/}+word.id.to_s+%{/ignore?src=page">x</a><br /><br />}
     @upwords = Word.find(:all, :conditions => ["user_id = ? and thumb_status = ? and sys_ignore_flag = ?", session[:user_id], "up", "no"], :order => "score DESC")
     if @upwords.size > 0
       i = 1
