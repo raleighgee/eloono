6325dfec16d33222f5bc2747f0d5bca56895fb55
rals0381 <raleigh.gresham@gmail.com>
2013-06-01 23:38:36 -0400

    update connections migration

--- db/migrate/20130519033148_create_connections.rb
+++ db/migrate/20130519033148_create_connections.rb
@@ -2,6 +2,7 @@ class CreateConnections < ActiveRecord::Migration
 	def change
 		create_table :connections do |t|
 			t.integer :user_id
+			t.string :connection_type, :default => "source" # mentioned, recommended, ignore
 			t.integer :twitter_id, :limit => 8
 			t.string :profile_image_url, :default => "wait"
 			t.string :user_name, :default => "not seen"
@@ -12,19 +13,21 @@ class CreateConnections < ActiveRecord::Migration
 			t.string :user_language, :default => "wait"
 			t.string :location, :default => "wait"
 			t.datetime :twitter_created_at
-			t.float :average_word_score, :default => 0
-			t.float :average_stream_word_score, :default => 0
-			t.float :appearances, :default => 0
-			t.string :earliest_tweets_at
-			t.string :latest_tweets_at
-			t.string :avg_tweets_at
-			t.float :total_tweets_seen
-			t.float :times_in_top, :default => 0
-			t.string :connection_type, :default => "mentioned" # mentioned, recommended, following, tagret, ignore
 			t.float :statuses_count
+			t.float :total_tweets_seen
 			t.float :followers_count
 			t.float :friends_count
 			t.float :tweets_per_hour
+			t.float :average_word_score, :default => 0
+			t.float :average_stream_word_score, :default => 0
+			t.float :appearances, :default => 0
+			t.float :overall_index, :default => 0
+			t.float :tone_score, :default => 0
+			t.float :ttwo_score, :default => 0
+			t.float :tthree_score, :default => 0
+			t.integer :tone_tweet_id, :default => 0, :limit => 8
+			t.integer :ttwo_tweet_id, :default => 0, :limit => 8
+			t.integer :tthree_tweet_id, :default => 0, :limit => 8
 			t.timestamps
 		end
 	end
